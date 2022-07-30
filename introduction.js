124c07b518e609bd77c7dca307a02efe2c987ad9
Eniola <enibusola@gmail.com>
2022-07-30 04:20:05 +0100

    A simple Javascript code

---
+++ introduction.js
@@ -0,0 +1,8 @@
+const info = {
+    name: "Eniola Ore-Aruwaji",
+    height: "5'6 ft",
+    country: "Nigeria",
+};
+
+
+alert(`Name: ${info.name} \nHeight: ${info.height} \nCountry: ${info.country}`);
